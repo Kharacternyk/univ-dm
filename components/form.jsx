ac2ef11107b37d6fe8ea2af5de421478beb2ddf4
Nazar Vinnichuk <nazar@vinnich.uk>
2025-03-01 22:02:44 +0200

    Add a plot.

--- components/form.jsx
+++ components/form.jsx
@@ -1,11 +1,13 @@
 import {Alert, Button, Stack, TextField} from "@mui/material";
 import {useCallback, useState} from "react";
+import Plot from "react-plotly.js";
 import {parseData, ParseError} from "../lib/parse-data";
 
 export const Form = () => {
   const [input, setInput] = useState("");
   const [data, setData] = useState(null);
   const [message, setMessage] = useState(null);
+  const [plot, setPlot] = useState(null);
 
   const inputHandler = useCallback((event) => {
     setInput(event.target.value);
@@ -26,10 +28,40 @@ export const Form = () => {
 
     setData(data);
     setMessage(null);
+
+    const columnCount = data[0].length;
+    const plotData = [];
+
+    for (const row of data) {
+      const label = row[row.length - 1];
+
+      if (plotData[label] === undefined) {
+        plotData[label] = {
+          x: [],
+          y: columnCount > 2 ? [] : undefined,
+          z: columnCount > 3 ? [] : undefined,
+          mode: "markers",
+          type:
+            columnCount > 3 ? "scatter3d" : columnCount > 2 ? "scatter" : "bar",
+        };
+      }
+
+      plotData[label].x.push(row[0]);
+
+      if (columnCount > 2) {
+        plotData[label].y.push(row[1]);
+      }
+
+      if (columnCount > 3) {
+        plotData[label].z.push(row[2]);
+      }
+    }
+
+    setPlot(<Plot data={plotData} />);
   }, [input]);
 
   return (
-    <Stack gap={2}>
+    <Stack gap={2} width="100%">
       <TextField
         value={input}
         onChange={inputHandler}
@@ -38,6 +70,7 @@ export const Form = () => {
       />
       {message}
       <Button onClick={parseHandler}>Parse</Button>
+      {plot}
     </Stack>
   );
 };
