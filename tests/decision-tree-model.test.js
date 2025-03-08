6de5d18c46b30d77b858c7bded296174d89afd1c
Nazar Vinnichuk <nazar@vinnich.uk>
2025-03-08 18:54:54 +0200

    Implement the decision tree model.

--- tests/decision-tree-model.test.js
+++ tests/decision-tree-model.test.js
@@ -1,5 +1,33 @@
 import {DecisionTreeModel} from "../lib/decision-tree-model";
 
+test("general decision tree", () => {
+  const model = new DecisionTreeModel([
+    [0, 0, 1, 0, 0],
+    [1, 0, 0, 1, 1],
+    [2, 0, 1, 0, 1],
+    [0, 1, 0, 0, 1],
+    [0, 1, 1, 0, 1],
+    [0, 1, 1, 1, 0],
+    [1, 0, 0, 1, 0],
+    [2, 0, 0, 0, 1],
+    [2, 1, 1, 0, 1],
+    [0, 1, 1, 1, 0],
+  ]);
+
+  expect(model.leafValue).toEqual(1);
+  expect(model.partitionColumnIndex).toEqual(0);
+  expect(model.children.get(0).partitionColumnIndex).toEqual(2 /* 3 - 1 */);
+  expect(model.children.get(0).children.get(0).partitionColumnIndex).toEqual(
+    0 /* 1 - 1 */
+  );
+
+  expect(model.predict([2, 1, 1, 1])).toEqual(1);
+  expect(model.predict([2, 0, 0, 0])).toEqual(1);
+  expect(model.predict([0, 1, 1, 1])).toEqual(0);
+  expect(model.predict([0, 0, 0, 1])).toEqual(0);
+  expect(model.predict([42, 0, 0, 0])).toEqual(1);
+});
+
 test("decision tree with one label", () => {
   const model = new DecisionTreeModel([
     [0, 0, 1, 0, 1],
